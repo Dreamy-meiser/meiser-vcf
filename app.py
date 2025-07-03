64266fbc07adb13fdbb03b3227665374dba9e279
Monteli Blessen <monteliblessen8@gmail.com>
2025-07-03 19:35:44 +0300

    Add Railway port support to Flask backend

--- app.py
+++ app.py
@@ -187,4 +187,5 @@ def handle_exception(e):
     return jsonify(error=str(e)), code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
